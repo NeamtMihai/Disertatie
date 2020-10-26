65ecd90aae8f7c32395d7b837169bce7661e4ee4
MihaiNeamtRedSharp <56109203+MihaiNeamtRedSharp@users.noreply.github.com>
2020-10-26 18:06:18 +0200

    added small node js express layer

---
+++ angular-front-end/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const http = require('http')
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const url = 'mongodb://localhost:27017';
+
+const app = express();
+
+const port = 3001
+
+
+app.get('/*', (req, res) => {
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+
+        if (err) throw err;
+
+        const db = client.db("articles");
+
+        db.collection('articles').find({}).toArray().then((docs) => {
+
+            // console.log(docs);
+            res.json(docs)
+
+        }).catch((err) => {
+
+            console.log(err);
+        }).finally(() => {
+
+            client.close();
+        });
+    });
+
+
+
+    // res.json("salut varule")
+}
+)
+
+const server = http.createServer(app)
+
+server.listen(port, () => {
+    console.log("merge serveru de mongo Varule...")
+})
+
+//to run this server tipe  node server.js
